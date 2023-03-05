e69a3c54e672c49e21556c794ccb38a4d8c8f3ad
ahmedsat <ahmedsat@ahmed.sat>
2023-03-05 13:40:12 +0200

    send vertex to engine

--- main.go
+++ main.go
@@ -10,9 +10,30 @@ import (
 //go:embed shaders
 var shaders embed.FS
 
+var (
+	red   = apps.Color{R: 1}
+	green = apps.Color{G: 1}
+	blue  = apps.Color{B: 1}
+)
+
+var vertices = []apps.Vertex{
+	{Vec2: apps.Vec2{X: 0.0, Y: -0.8}, Color: red},
+	{Vec2: apps.Vec2{X: 0.8, Y: 0.8}, Color: green},
+	{Vec2: apps.Vec2{X: -0.8, Y: 0.8}, Color: blue},
+
+	{Vec2: apps.Vec2{X: -0.5, Y: -0.5}, Color: red},
+	{Vec2: apps.Vec2{X: 0.5, Y: -0.5}, Color: green},
+	{Vec2: apps.Vec2{X: 0.5, Y: 0.5}, Color: blue},
+
+	{Vec2: apps.Vec2{X: 0.5, Y: 0.5}, Color: blue},
+	{Vec2: apps.Vec2{X: -0.5, Y: 0.5}, Color: green},
+	{Vec2: apps.Vec2{X: -0.5, Y: -0.5}, Color: red},
+}
+
 func main() {
 	app := &apps.HelloTriangleApplication{
-		Shaders: shaders,
+		Shaders:  shaders,
+		Vertices: vertices,
 	}
 
 	err := app.Run()
