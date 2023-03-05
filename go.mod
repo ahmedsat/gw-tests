1820ebd326ab6641b5434fb221aee3ee037ca0ac
ahmedsat <ahmedsat@ahmed.sat>
2023-03-05 16:30:42 +0200

    index buffer

--- go.mod
+++ go.mod
@@ -2,9 +2,28 @@ module github.com/ahmedsat/gw-tests
 
 go 1.19
 
-require github.com/ahmedsat/gw-engine v0.0.0-20230303130709-1185ea124e22
+require (
+	github.com/ahmedsat/go-math-helper v0.0.0-20230305130824-9058fa94c9c0
+	github.com/ahmedsat/gw-engine v0.0.0-20230305113933-e2eb0075e11b
+)
 
 require (
+	github.com/CannibalVox/cgoparam v1.0.0 // indirect
 	github.com/TwiN/go-color v1.4.0 // indirect
+	github.com/cockroachdb/errors v1.9.0 // indirect
+	github.com/cockroachdb/logtags v0.0.0-20211118104740-dabe8e521a4f // indirect
+	github.com/cockroachdb/redact v1.1.3 // indirect
+	github.com/getsentry/sentry-go v0.16.0 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/kr/pretty v0.3.1 // indirect
+	github.com/kr/text v0.2.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/rogpeppe/go-internal v1.9.0 // indirect
 	github.com/veandco/go-sdl2 v0.4.33 // indirect
+	github.com/vkngwrapper/core/v2 v2.1.1 // indirect
+	github.com/vkngwrapper/extensions/v2 v2.1.0 // indirect
+	github.com/vkngwrapper/integrations/sdl2/v2 v2.0.2 // indirect
+	golang.org/x/sys v0.3.0 // indirect
+	golang.org/x/text v0.5.0 // indirect
 )
--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"embed"
 
+	"github.com/ahmedsat/go-math-helper/math32/colors"
+	"github.com/ahmedsat/go-math-helper/math32/vectors"
 	"github.com/ahmedsat/gw-engine/apps"
 	"github.com/ahmedsat/gw-engine/log"
 )
@@ -11,29 +13,31 @@ import (
 var shaders embed.FS
 
 var (
-	red   = apps.Color{R: 1}
-	green = apps.Color{G: 1}
-	blue  = apps.Color{B: 1}
+	red   = colors.Color{R: 1}
+	green = colors.Color{G: 1}
+	blue  = colors.Color{B: 1}
+	yalow = colors.Color{R: 1, G: 1}
 )
 
+var indices = []uint16{0, 1, 2, 2, 3, 0}
+
 var vertices = []apps.Vertex{
-	{Vec2: apps.Vec2{X: 0.0, Y: -0.8}, Color: red},
-	{Vec2: apps.Vec2{X: 0.8, Y: 0.8}, Color: green},
-	{Vec2: apps.Vec2{X: -0.8, Y: 0.8}, Color: blue},
-
-	{Vec2: apps.Vec2{X: -0.5, Y: -0.5}, Color: red},
-	{Vec2: apps.Vec2{X: 0.5, Y: -0.5}, Color: green},
-	{Vec2: apps.Vec2{X: 0.5, Y: 0.5}, Color: blue},
-
-	{Vec2: apps.Vec2{X: 0.5, Y: 0.5}, Color: blue},
-	{Vec2: apps.Vec2{X: -0.5, Y: 0.5}, Color: green},
-	{Vec2: apps.Vec2{X: -0.5, Y: -0.5}, Color: red},
+	// {Vec3: vectors.Vec3{X: 0.0, Y: -0.8}, Color: red},
+	// {Vec3: vectors.Vec3{X: 0.8, Y: 0.8}, Color: green},
+	// {Vec3: vectors.Vec3{X: -0.8, Y: 0.8}, Color: blue},
+	{Vec3: vectors.Vec3{X: -0.5, Y: -0.5}, Color: red},
+	{Vec3: vectors.Vec3{X: 0.5, Y: -0.5}, Color: green},
+	{Vec3: vectors.Vec3{X: 0.5, Y: 0.5}, Color: blue},
+	// {Vec3: vectors.Vec3{X: 0.5, Y: 0.5}, Color: blue},
+	{Vec3: vectors.Vec3{X: -0.5, Y: 0.5}, Color: yalow},
+	// {Vec3: vectors.Vec3{X: -0.5, Y: -0.5}, Color: red},
 }
 
 func main() {
 	app := &apps.HelloTriangleApplication{
 		Shaders:  shaders,
 		Vertices: vertices,
+		Indices:  indices,
 	}
 
 	err := app.Run()
